675be5449c346dc4b6d86e79ec9d7dac49566312
Haris Sheikh <harissheikh@Hariss-MacBook-Pro.local>
2019-07-01 11:23:50 -0400

    code review feedback

--- build/evergreen_config_lib/tasks.py
+++ build/evergreen_config_lib/tasks.py
@@ -491,7 +491,7 @@ all_tasks = chain(all_tasks, DNSTask.matrix())
 
 
 class CompressionTask(MatrixTask):
-    axes = OD([('compression', ['zlib', 'snappy', 'zstd', 'compression'])])
+    axes = OD([('compression', ['zlib', 'snappy', 'zstd', 'compression', '.compressors'])])
     name_prefix = 'test-latest-server'
 
     def __init__(self, *args, **kwargs):
